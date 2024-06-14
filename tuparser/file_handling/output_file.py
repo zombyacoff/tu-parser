850cc4b02d4b968ede213edac7c1f3f22eda3acb
zombyacoff <zombyacoff@gmail.com>
2024-06-15 00:09:47 +0300

    refactor(output_file): write_data simplification

--- tuparser/file_handling/output_file.py
+++ tuparser/file_handling/output_file.py
@@ -16,24 +16,17 @@ class YAMLOutputFile:
         self.file_path = FileManager.join_paths(self.folder_path, self.name)
 
     @call_counter
-    def write_data(self, data: any) -> None:
+    def write_data(self, *data: any) -> None:
         """Writes data to the dictionary 'data'
         
         :param data: data to write. If multiple values need to be written,
         they should be passed as a tuple.
         
         Example:
-            write_data(('Alexey', 'Yaroslav', 'TUParser')) - multiple values are passed,
-            and they are written to the dictionary keys in order: 
-            'Alexey' to the first key, 'Yaroslav' to the second, 'TUParser' to the third.
-            NOTE: the dictionary should have at least as many keys as the values passed.
-    
-            write_data('Alexey') - a single value, written to the 1st key of the dictionary. 
-            No further operations are performed.
+            write_data('Alexey', 'Yaroslav', 'TUParser')
+        
+        NOTE: the dictionary should have at least as many keys as the values passed.
         """
-        if not isinstance(data, tuple):
-            self.data[list(data.keys())[0]] = data
-            return
         for i, key in enumerate(self.data):
             self.data[key][self.write_data.calls] = data[i]
 
