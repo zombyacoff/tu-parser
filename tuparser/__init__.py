2a1ad673ce2c0784d92abbc4b1528046b62647bc
zombyacoff <zombyacoff@gmail.com>
2024-06-16 23:57:21 +0300

    build: update 0.1.5.1

--- tuparser/__init__.py
+++ tuparser/__init__.py
@@ -5,7 +5,7 @@ from .parser import *
 from .utils import *
 from .validator import *
 
-__version__ = "0.1.5"
+__version__ = "0.1.5.1"
 __author__ = "zombyacoff"
 __email__ = "zombyacoff@gmail.com"
 __git_url__ = "https://github.com/zombyacoff/tu-parser"
