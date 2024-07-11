2cb53069d73d70021cad35945847d65763cd1ef7
zombyacoff <zombyacoff@gmail.com>
2024-07-11 18:56:09 +0300

    refactor: remove useless imports from utils
    also change graph url const name to TELEGRAPH_MIRROR_URL

--- tuparser/__init__.py
+++ tuparser/__init__.py
@@ -1,23 +1,17 @@
 from .config import TelegraphParserConfig
-from .constants import LAUNCH_TIME, TELEGRAPH_URL, TELEGRAPH_URL_MIRROR
+from .constants import TELEGRAPH_MIRROR_URL, TELEGRAPH_URL
 from .file_handling import FileManager, YAMLOutputFile
 from .parser import TelegraphParser, run_parser
-from .utils import ConsoleColor, call_counter, get_monthrange, get_time_now
 from .validator import validate
 
 __all__ = [
     "TelegraphParserConfig",
-    "LAUNCH_TIME",
+    "TELEGRAPH_MIRROR_URL",
     "TELEGRAPH_URL",
-    "TELEGRAPH_URL_MIRROR",
     "FileManager",
     "YAMLOutputFile",
     "TelegraphParser",
     "run_parser",
-    "ConsoleColor",
-    "get_monthrange",
-    "get_time_now",
-    "call_counter",
     "validate",
 ]
 
--- tuparser/constants.py
+++ tuparser/constants.py
@@ -3,4 +3,4 @@ from .utils import get_time_now
 LAUNCH_TIME = get_time_now()
 
 TELEGRAPH_URL = "https://telegra.ph"
-TELEGRAPH_URL_MIRROR = "https://graph.org"
+TELEGRAPH_MIRROR_URL = "https://graph.org"
