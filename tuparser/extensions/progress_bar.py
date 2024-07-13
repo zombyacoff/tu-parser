9e6b5b81760e21951dc3bacfc72518d189349c9d
zombyacoff <zombyacoff@gmail.com>
2024-07-13 19:30:34 +0300

    refactor: remove some consts from progress_bar

--- tuparser/extensions/progress_bar.py
+++ tuparser/extensions/progress_bar.py
@@ -1,8 +1,6 @@
 FULL_CHAR = "█"
 HALF_CHAR = "▒"
 
-TOTAL_PROGRESS = "[{current}/{total}]"  # [15/100]
-PERCENT_PROGRESS = "[{percent:.2f}%]"  # [15.00%]
 RAW_PROGRESS_BAR_LENGTH = 64
 
 
@@ -12,12 +10,7 @@ class ProgressBar:
         percent = 100 * current / total
         current_bar_length = round(percent) // 2
         bar = current_bar_length * FULL_CHAR + (50 - current_bar_length) * HALF_CHAR
-        print(
-            bar,
-            TOTAL_PROGRESS.format(current=current, total=total),
-            PERCENT_PROGRESS.format(percent=percent),
-            end="\r",
-        )
+        print(bar, "[{current}/{total}]", "[{percent:.2f}%]", end="\r")
 
     @staticmethod
     def get_length(total: int) -> int:
