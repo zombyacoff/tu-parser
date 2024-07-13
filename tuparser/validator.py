5462027710d50f8e89ec4012134491c1113edd10
zombyacoff <zombyacoff@gmail.com>
2024-07-13 19:20:42 +0300

    refactor: remove messages consts from validator

--- tuparser/validator.py
+++ tuparser/validator.py
@@ -4,12 +4,6 @@ from .exceptions import ValidatorException
 OFFSET_RANGE = (1, 250)
 RELEASE_DATES_RANGE = (0, LAUNCH_TIME.year)
 
-INVALID_TITLES_MESSAGE = "Invalid titles: {}\nValues must be a list without None"
-INVALID_OFFSET_MESSAGE = "Invalid offset: {}\nValue must be an integer and must be between 1 and 250 inclusive"
-INVALID_PROGRESS_BAR_MESSAGE = "Invalid progress bar value: {}\nValue must be a boolean"
-INVALID_RELEASE_DATES_MESSAGE = "Invalid release dates: {}\nValues must be a list of integers and must be within the specified range [0, LAUNCH_TIME_YEAR]"
-INVALID_MESSAGES_MESSAGE = "Invalid messages value: {}\nValue must be a boolean"
-
 
 def check_range(value: int, value_range: tuple[int, int]) -> bool:
     return value_range[0] <= value <= value_range[1]
@@ -35,11 +29,31 @@ def list_wtn(values: any) -> bool:
 
 def validate(*parser_args: tuple[any]) -> tuple[any]:
     validate_dict = {
-        "titles": (parser_args[0], list_wtn, INVALID_TITLES_MESSAGE),
-        "messages": (parser_args[1], boolean, INVALID_MESSAGES_MESSAGE),
-        "offset": (parser_args[2], integer, INVALID_OFFSET_MESSAGE),
-        "progress_bar": (parser_args[3], boolean, INVALID_PROGRESS_BAR_MESSAGE),
-        "release_dates": (parser_args[4], list_int, INVALID_RELEASE_DATES_MESSAGE),
+        "titles": (
+            parser_args[0],
+            list_wtn,
+            "Invalid titles: {}\nValues must be a list without None",
+        ),
+        "messages": (
+            parser_args[1],
+            boolean,
+            "Invalid offset: {}\nValue must be an integer and must be between 1 and 250 inclusive",
+        ),
+        "offset": (
+            parser_args[2],
+            integer,
+            "Invalid progress bar value: {}\nValue must be a boolean",
+        ),
+        "progress_bar": (
+            parser_args[3],
+            boolean,
+            "Invalid release dates: {}\nValues must be a list of integers and must be within the specified range [0, LAUNCH_TIME_YEAR]",
+        ),
+        "release_dates": (
+            parser_args[4],
+            list_int,
+            "Invalid messages value: {}\nValue must be a boolean",
+        ),
     }
     for params in validate_dict.values():
         value, validate_function, exception_message = params
