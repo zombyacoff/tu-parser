688a9b3e617536af87956111d81c2446c468f8b9
zombyacoff <zombyacoff@gmail.com>
2024-07-16 22:25:00 +0300

    fix: output_file in word finder example

--- examples/word_finder.py
+++ examples/word_finder.py
@@ -1,13 +1,9 @@
-from tuparser import TelegraphParser, YAMLOutputFile, run_parser
+from tuparser import TelegraphParser, run_parser
 
 WORD = "dog"
 
 
 class WordFinder(TelegraphParser):
-    def __init__(self, parser_args, output_file):
-        super().__init__(parser_args)
-        self.output_file = output_file
-
     async def parse(self, url, soup):
         website_text = list(soup.stripped_strings)
         for sentence in website_text:
@@ -15,5 +11,4 @@ class WordFinder(TelegraphParser):
                 self.output_file.write_data(url)
 
 
-output_file = YAMLOutputFile({"url": {}})
-run_parser(WordFinder, titles=["PLACEHOLDER"], custom_args=[output_file])
+run_parser(WordFinder, titles=["PLACEHOLDER"], output_file=[{"url": {}}])
