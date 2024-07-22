335c7abad0d7dead4fd03920a6b432910a4753e1
zombyacoff <zombyacoff@gmail.com>
2024-07-22 23:46:47 +0300

    build: version 0.2.0 -> 0.2.1

--- tuparser/__init__.py
+++ tuparser/__init__.py
@@ -4,7 +4,7 @@ from .parser import TelegraphParser, run_parser
 __all__ = ["TELEGRAPH_MIRROR_URL", "TELEGRAPH_URL", "TelegraphParser", "run_parser"]
 
 # This information is used in the setup.py file:
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "zombyacoff"
 __email__ = "zombyacoff@gmail.com"
 __git_url__ = "https://github.com/zombyacoff/tu-parser"
