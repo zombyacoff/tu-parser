3bca3a4c204aa41c7bcd95819226e63db1430acf
zombyacoff <zombyacoff@gmail.com>
2024-08-21 02:28:41 +0300

    remove leap year const

--- tuparser/utils.py
+++ tuparser/utils.py
@@ -1,12 +1,10 @@
 from calendar import monthrange
 from datetime import datetime
 
-LEAP_YEAR = 2020
-
 
 def get_time_now() -> datetime:
     return datetime.now()
 
 
 def get_days_in_month(month: int) -> int:
-    return monthrange(LEAP_YEAR, month)[1]
+    return monthrange(2020, month)[1]
